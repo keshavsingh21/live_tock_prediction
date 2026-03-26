f1bea48a6c8e32ef50323d00c80514cd64fd59f8
keshavsingh21 <168504764+keshavsingh21@users.noreply.github.com>
2026-03-26 10:19:31 +0530

    Update app.py

--- app.py
+++ app.py
@@ -141,7 +141,7 @@ with st.spinner(f"🔄 Loading {selected_company} data & training model..."):
 
 
     try:
-        model = load_model("model.keras")
+        model = load_model("model.keras",compile = False)
         scaler = joblib.load("scaler.pkl")
     except Exception as e:
         st.error(f"❌ Actual error: {e}")
